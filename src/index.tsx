6686fa45419dc32157457480a32abd45b1d5a3bf
Armen <armen21mkrtchyan0616@gmail.com>
2022-11-23 21:21:35 +0400

    add i18n

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import ReactDOM from 'react-dom/client';
 import App from 'AppContainer';
+import './i18n';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
     <App />
 );
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,16 +1,17 @@
 import { login } from "features/user"
 import { useDispatch } from "react-redux"
+import { useTranslation } from 'react-i18next';
 
 const Login=()=>{
-
-
+const {t,i18n}=useTranslation()
     const dispatch = useDispatch()
     const handleLogIn=()=>{
        dispatch(login({login:"armen21",passowrd:'Armen.21.06'})) 
     }
     
     return<div>
-        <button onClick={handleLogIn}>Login</button>
+        <button onClick={handleLogIn}>{t('login')}</button>
+        <button onClick={()=>{i18n.changeLanguage('hy')}}>Change Language</button>
         </div>
 }
 
